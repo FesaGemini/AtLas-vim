09ceebfb298131945f7d57b3feb0edec91e27cfa
Clark Steven <fesademini@gmail.com>
2022-03-14 08:03:24 +0700

    Update install.sh

--- lua/core/install/install.sh
+++ lua/core/install/install.sh
@@ -1,8 +1,12 @@
+sleep 10 &
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim;
 
+sleep 5 &
 mv ~/.config/nvim ~/.config/nvim.bck;
 
+sleep 10 &
 git clone https://github.com/FesaGemini/AtLas-vim.git ~/.config/nvim;
 
+sleep 5 &
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync;
