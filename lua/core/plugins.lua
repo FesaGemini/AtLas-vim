29980fc0b22492c50ec7e229103ff878227dc95a
FesaGemini <fesagemini@gmail.com>
2022-03-15 08:50:17 +0700

    added signature help

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -17,6 +17,7 @@ return packer.startup({function(use)
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
   }
 
   -- Snippet
